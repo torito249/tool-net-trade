d5fc2d8789941e61ae98f53a3b1d432254b0be78
torito249 <info@tool-net.jp>
2024-07-30 00:30:17 +0900

    Initial commit

---
+++ next/tool-net-trade/src/app/page.tsx
@@ -0,0 +1,17 @@
+// src/app/page.tsx
+
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My New Title</title>
+      </Head>
+      <main>
+        <h1>Welcome to My New Next.js App!</h1>
+        <p>This is the updated content of the homepage.</p>
+      </main>
+    </div>
+  );
+}
